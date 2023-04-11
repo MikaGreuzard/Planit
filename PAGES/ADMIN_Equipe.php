8752747c94a38e39ae7d941cae05e70d13fc458a
MikaGreuzard <114180139+MikaGreuzard@users.noreply.github.com>
2023-04-11 17:06:37 +0200

    v1.08

--- PAGES/ADMIN_Equipe.php
+++ PAGES/ADMIN_Equipe.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <!--<title> Responsive Sidebar Menu  | CodingLab </title>-->
     <link rel="stylesheet" href="../CSS/ADMIN_Equipe.css">
-    <title>Plan</title> <!-- Titre de la page -->
+    <title>Plan'it</title> <!-- Titre de la page -->
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
